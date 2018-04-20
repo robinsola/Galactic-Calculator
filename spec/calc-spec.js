803a97e7a97996e8a3f6454826dd371f69905317
Guest User <Guest@Epicodus-6A.local>
2018-04-20 10:16:45 -0700

    run passing test to calculate earth years.

--- spec/calc-spec.js
+++ spec/calc-spec.js
@@ -3,7 +3,7 @@ import { Age } from './../src/calc.js';
 describe('Age', function() {
   it('should calculate accurate age in Earth years from user birthdate', function() {
     let age = new Age("March 20 1986")
-    expect(age.earthYears()).toEqual(3)
+    expect(age.earthYears()).toEqual(32)
   });
 
 })
