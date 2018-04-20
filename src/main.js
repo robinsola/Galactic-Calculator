fd48c6b63dc90ac50055e217ffd40a16a24f9b77
Guest User <Guest@Epicodus-6A.local>
2018-04-20 16:46:39 -0700

    add output div to html and user logic to put variables into constructor.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-// import { title } from './title';
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
+import { Age } from './calc';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+  $("#calculator").submit(function(event) {
+    event.preventDefault();
+
+    let inputBirth = $("#birthdate").val();
+    let lifeExpectAge = parseInt($("#life-expectancy").val());
+    let newAge = new Age(inputBirth, lifeExpectAge);
+
+    $(".output").text("You are " + newAge.exactSeconds() + " seconds old.")
+
+  });
+});
