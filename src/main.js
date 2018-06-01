0e743cb4ec866efbcb10e732d98415cc2956106f
Robin Sola <rsolarms@gmail.com>
2018-05-31 18:55:09 -0700

    add functions to show results

--- src/main.js
+++ src/main.js
@@ -7,12 +7,25 @@ import './styles.css';
 $(document).ready(function() {
   $("#calculator").submit(function(event) {
     event.preventDefault();
-
+    let userName = $("#user-name").val();
     let inputBirth = $("#birthdate").val();
     let lifeExpectAge = parseInt($("#life-expectancy").val());
     let newAge = new Age(inputBirth, lifeExpectAge);
-
-    $(".output").text("You are " + newAge.exactSeconds() + " seconds old.")
-
+    newAge.ageTimer();
+    $(".wrapper").hide();
+    $(".timer").show();
+    $(".output").show();
+    setInterval(() => {
+      $("#timer").text(newAge.counter)
+    }, 1000);
+    $(".name-output").text(userName);
+    $(".earth-age").text(newAge.earthYears());
+    $(".mercury-age").text(newAge.mercuryYears());
+    $(".venus-age").text(newAge.venusYears());
+    $(".jupiter-age").text(newAge.jupiterYears());
+    $(".earth-death").text(lifeExpectAge - newAge.earthAge);
+    $(".mercury-death").text(newAge.mercuryDeath());
+    $(".venus-death").text(newAge.venusDeath());
+    $(".jupiter-death").text(newAge.jupiterDeath());
   });
 });
